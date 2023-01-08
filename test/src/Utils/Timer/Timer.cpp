ab1adb04845f93340e8c050d24330aa21f6e40cb
Breno Oliveira Rodrigues <bor@cin.ufpe.br>
2023-01-08 08:33:49 -0300

    feat(utils): ParameterHandler tests

---
+++ test/src/Utils/Timer/Timer.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+#include <gtest/gtest-spi.h>
+#include <gmock/gmock.h>
+
+#include <spdlog/spdlog.h>
+#include <spdlog/fmt/bundled/color.h>
+
+#include <Armorial/Utils/Timer/Timer.h>
+
