abf6e0084277f0a55171b227e2ec8dc625b2281d
Breno Oliveira Rodrigues <bor@cin.ufpe.br>
2023-01-09 20:58:05 -0300

    feat(utils): Timer initial tests

--- test/src/Utils/Timer/Timer.cpp
+++ test/src/Utils/Timer/Timer.cpp
@@ -7,3 +7,28 @@
 
 #include <Armorial/Utils/Timer/Timer.h>
 
+class TimerMock : public Utils::Timer {
+public:
+    std::chrono::time_point<std::chrono::system_clock> getStartPoint() {
+        return _startPoint;
+    }
+
+    std::chrono::time_point<std::chrono::system_clock> getEndPoint() {
+        return _endPoint;
+    }
+
+private:
+    std::chrono::time_point<std::chrono::system_clock> _startPoint;
+    std::chrono::time_point<std::chrono::system_clock> _endPoint;
+};
+
+TEST(Utils_Timer_Test, When_Creating_Timer_Should_Set_Points_To_Now) {
+    TimerMock timer = TimerMock();
+    std::chrono::time_point<std::chrono::system_clock> clock = std::chrono::system_clock::now();
+
+    double startDiff = std::chrono::duration_cast<std::chrono::nanoseconds>(clock - timer.getStartPoint()).count();
+    double endDiff = std::chrono::duration_cast<std::chrono::nanoseconds>(clock - timer.getEndPoint()).count();
+
+    EXPECT_NEAR(startDiff/1E18, 0.0, 10.0);
+    EXPECT_NEAR(endDiff/1E18, 0.0, 10.0);
+}
