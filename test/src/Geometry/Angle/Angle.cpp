cdf22fc163c3060e403078c2490eb3e6b04b8542
ViníciusAlvesPereira <viniciusvap1999@gmail.com>
2023-01-22 15:53:56 -0300

    feat(test): Adding new Angle testes

--- test/src/Geometry/Angle/Angle.cpp
+++ test/src/Geometry/Angle/Angle.cpp
@@ -2,39 +2,68 @@
 #include <gmock/gmock.h>
 
 #include <Armorial/Geometry/Angle/Angle.h>
+#include <Armorial/Utils/Utils.h>
 #include <spdlog/spdlog.h>
 #include <spdlog/fmt/bundled/color.h>
 
-TEST(Geometry_Angle_Tests, MinimumAngleLimitTest) {
+
+TEST(Geometry_Angle_Tests, GivenAnAngle_AngleLimitsChecker_ShouldWork) {
     Geometry::Angle angleBase(0.0f);
-    EXPECT_GE(angleBase.value(), -M_PI) << "Angle under minimum angle limit";
+
+    ASSERT_GE(angleBase.value(), -M_PI) << "Angle under minimum angle limit";
+    ASSERT_LE(angleBase.value(), M_PI) << "Angle over maximum angle limit";
+    EXPECT_EQ(Utils::Compare::isEqual(angleBase.value(), 0.0f), true) << "Angle value does not match";
 }
 
-TEST(Geometry_Angle_Tests, MaximumAngleLimitTest) {
-    Geometry::Angle angleBase(0.0f);
-    EXPECT_LE(angleBase.value(), M_PI) << "Angle over maximum angle limit";
+TEST(Geometry_Angle_Tests, GivenAnAngle_Normalization_ShouldWork) {
+    Geometry::Angle angleBase(M_PI_4);
+    Geometry::Angle referenceAngle1(17 * M_PI_4);
+    Geometry::Angle referenceAngle2(-7 * M_PI_4);
+
+    EXPECT_EQ(angleBase, referenceAngle1) << "Normalization does not work";
+    EXPECT_EQ(angleBase, referenceAngle2) << "Normalization does not work";
 }
 
-TEST(Geometry_Angle_Tests, OperatorsTest) {
+TEST(Geometry_Angle_Tests, GivenAnAngle_RotationDirectionGetter_ShouldWork) {
+    Geometry::Angle angleBase(0.0f);
+
+    EXPECT_EQ(angleBase.rotateDirection(M_PI - 0.1f), Geometry::Angle::COUNTER_CLOCKWISE) << "Rot. direction does not match";
+    EXPECT_EQ(angleBase.rotateDirection(-sqrtf(3)), Geometry::Angle::CLOCKWISE) << "Rot. direction does not match";
+}
+
+TEST(Geometry_Angle_Tests, GivenAnAngle_ShotestAngleGetter_ShouldWork) {
+    Geometry::Angle angleBase(M_PI);
+    EXPECT_FLOAT_EQ(angleBase.shortestAngleDiff(sqrtf(3)), M_PI - sqrtf(3)) << "Angle difference does not match";
+    EXPECT_FLOAT_EQ(angleBase.shortestAngleDiff(3*M_PI_2), M_PI_2) << "Angle difference does not match";
+}
+
+TEST(Geometry_Angle_Tests, GivenAnAngle_Operators_ShouldWork) {
     Geometry::Angle angleBase(0.0f);
 
     {
-        SCOPED_TRACE("Angles sum");
-
-        Geometry::Angle angleSum(3.5f - 2*M_PI);
-        EXPECT_EQ(angleBase + Geometry::Angle(3.5f) == angleSum, true) << "AngleBase: " << angleBase.value();
+        SCOPED_TRACE("Angle addictors");
+        Geometry::Angle referenceAngle(-sqrtf(3));
+        Geometry::Angle angleSum = referenceAngle + Geometry::Angle(sqrtf(3));
+        EXPECT_EQ(angleSum, angleBase) << "Addition operators do not work";
+        EXPECT_EQ(referenceAngle += Geometry::Angle(sqrtf(3)), angleBase) << "Addition operators do not work";
     }
     {
-        SCOPED_TRACE("Angles subtrator");
-
-        Geometry::Angle angleSubtractor(2*M_PI - 3.5f);
-        EXPECT_EQ(angleBase - Geometry::Angle(3.5f) == angleSubtractor, true) << "AngleBase: " << angleBase.value();
+        SCOPED_TRACE("Angle subtrators");
+        Geometry::Angle referenceAngle(sqrtf(3));
+        Geometry::Angle angleSubtraction = referenceAngle - Geometry::Angle(sqrtf(3));
+        EXPECT_EQ(angleSubtraction, angleBase) << "Subtraction operators do not work";
+        EXPECT_EQ(referenceAngle -= Geometry::Angle(sqrtf(3)), angleBase) << "Addition operators do not work";
     }
     {
-        SCOPED_TRACE("Angle equalization");
-
-        Geometry::Angle newAngle(3.5f -2*M_PI);
-        angleBase = 3.5f;
-        EXPECT_EQ(angleBase == newAngle, true) << "AngleBase: " << angleBase.value();
+        SCOPED_TRACE("Angle comparison");
+        EXPECT_EQ(angleBase == Geometry::Angle(0.0f), true) << "Equality operator does not work";
+        EXPECT_EQ(angleBase != Geometry::Angle(sqrtf(3)), true) << "Inequality operator does not work";
+    }
+    {
+        SCOPED_TRACE("Angle assignment");
+        Geometry::Angle referenceAngle = sqrtf(3);
+        EXPECT_NE(angleBase, referenceAngle) << "Angles should not match";
+        referenceAngle = 0.0f;
+        EXPECT_EQ(angleBase, referenceAngle) << "Angles should match";
     }
 }
