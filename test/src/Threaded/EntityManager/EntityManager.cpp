d3b065fb9467bec4475678a22064ab23eab36d96
Breno Oliveira Rodrigues <bor@cin.ufpe.br>
2023-01-28 23:38:07 -0300

    EntityManager and Timer Tests

--- test/src/Threaded/EntityManager/EntityManager.cpp
+++ test/src/Threaded/EntityManager/EntityManager.cpp
@@ -5,30 +5,111 @@
 #include <spdlog/spdlog.h>
 #include <spdlog/fmt/bundled/color.h>
 
+#include <thread>
+
 #include <src/Threaded/EntityCommons.h>
 
 #include <Armorial/Threaded/EntityManager/EntityManager.h>
 
 TEST(Threaded_Entity_Manager_Test, When_Adding_New_Entity_Should_Store_With_Correct_Priority) {
-//    EntityCommons::EntityMock *entidade1 = (EntityCommons::EntityMock*) new EntityCommons::Entidade();
-//    EntityCommons::EntityMock *entidade2 = (EntityCommons::EntityMock*) new EntityCommons::Teste();
-//    EntityCommons::EntityMock *entidade3 = (EntityCommons::EntityMock*) new EntityCommons::Fon();
+    testing::NiceMock<EntityCommons::TesteMock> *entidade1 = new testing::NiceMock<EntityCommons::TesteMock>();
+    testing::NiceMock<EntityCommons::Teste2Mock> *entidade2 = new testing::NiceMock<EntityCommons::Teste2Mock>();
 
-//    EntityCommons::ManagerMockable *manager = new EntityCommons::ManagerMockable();
+    EntityCommons::ManagerMockable *manager = new EntityCommons::ManagerMockable();
 
-//    testing::ExpectationSet entity1;
-//    entity1 += EXPECT_CALL(*entidade1, initialization()).Times(testing::AtLeast(1));
+    testing::ExpectationSet exptSet;
+    exptSet += EXPECT_CALL(*entidade1, initialization()).Times(testing::AtLeast(1));
 
-//    testing::ExpectationSet entity2;
-//    entity2 += EXPECT_CALL(*entidade2, initialization()).Times(testing::AtLeast(1)).After(entity1);
+    // Never used, but needed to be created as an object to work properly
+    testing::Expectation expectation = EXPECT_CALL(*entidade2, initialization()).Times(testing::AtLeast(1)).After(exptSet);
+//    EXPECT_CALL(*entidade2, initialization()).Times(testing::AtLeast(1)).After(exptSet);
 
-//    testing::ExpectationSet entity3;
-//    entity3 += EXPECT_CALL(*entidade3, initialization()).Times(testing::AtLeast(1)).After(entity2);
+    manager->addEntity(entidade1, 0);
+    manager->addEntity(entidade2, 1);
 
-//    manager->addEntity(entidade1, 0);
-//    manager->addEntity(entidade2, 1);
-//    manager->addEntity(entidade3, 2);
+    manager->startEntities();
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    manager->disableEntities();
+}
 
-//    manager->startEntities();
-//    manager->disableEntities();
+TEST(Threaded_Entity_Manager_Test, When_Starting_Entities_All_Initialization_Should_Be_Called) {
+    testing::NiceMock<EntityCommons::TesteMock> *entidadeLoop1 = new testing::NiceMock<EntityCommons::TesteMock>();
+    testing::NiceMock<EntityCommons::Teste2Mock> *entidadeLoop2 = new testing::NiceMock<EntityCommons::Teste2Mock>();
+
+    EntityCommons::ManagerMockable *manager = new EntityCommons::ManagerMockable();
+
+    EXPECT_CALL(*entidadeLoop1, initialization()).Times(testing::AtLeast(1));
+
+    EXPECT_CALL(*entidadeLoop2, initialization()).Times(testing::AtLeast(1));
+
+    manager->addEntity(entidadeLoop1, 0);
+    manager->addEntity(entidadeLoop2, 0);
+
+    manager->startEntities();
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    manager->disableEntities();
+}
+
+TEST(Threaded_Entity_Manager_Test, After_Start_Entities_All_Loops_Should_Be_Called) {
+    testing::NiceMock<EntityCommons::TesteMock> *entidadeLoop1 = new testing::NiceMock<EntityCommons::TesteMock>();
+    testing::NiceMock<EntityCommons::Teste2Mock> *entidadeLoop2 = new testing::NiceMock<EntityCommons::Teste2Mock>();
+
+    EntityCommons::ManagerMockable *manager = new EntityCommons::ManagerMockable();
+
+    EXPECT_CALL(*entidadeLoop1, loop()).Times(testing::AtLeast(1));
+
+    EXPECT_CALL(*entidadeLoop2, loop()).Times(testing::AtLeast(1));
+
+    manager->addEntity(entidadeLoop1, 0);
+    manager->addEntity(entidadeLoop2, 0);
+
+    manager->startEntities();
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    manager->disableEntities();
+}
+
+TEST(Threaded_Entity_Manager_Test, After_Run_When_Disabling_Entities_All_Finalizations_Should_Be_Called) {
+    testing::NiceMock<EntityCommons::TesteMock> *entidade1 = new testing::NiceMock<EntityCommons::TesteMock>();
+    testing::NiceMock<EntityCommons::Teste2Mock> *entidade2 = new testing::NiceMock<EntityCommons::Teste2Mock>();
+
+    EntityCommons::ManagerMockable manager = EntityCommons::ManagerMockable();
+
+    EXPECT_CALL(*entidade1, finalization()).Times(testing::AtLeast(1));
+
+    EXPECT_CALL(*entidade2, finalization()).Times(testing::AtLeast(1));
+
+    manager.addEntity(entidade1, 0);
+    manager.addEntity(entidade2, 0);
+
+    manager.startEntities();
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    manager.disableEntities();
+}
+
+TEST(Threaded_Entity_Manager_Test, When_Running_Entities_It_Should_Respect_Its_Flow) {
+    testing::NiceMock<EntityCommons::TesteMock> *entidade1 = new testing::NiceMock<EntityCommons::TesteMock>();
+    testing::NiceMock<EntityCommons::Teste2Mock> *entidade2 = new testing::NiceMock<EntityCommons::Teste2Mock>();
+
+    EntityCommons::ManagerMockable manager = EntityCommons::ManagerMockable();
+
+    testing::ExpectationSet exptSet1;
+    testing::ExpectationSet exptSet2;
+    exptSet1 += EXPECT_CALL(*entidade1, initialization()).Times(testing::AtLeast(1));
+
+    exptSet2 += EXPECT_CALL(*entidade2, initialization()).Times(testing::AtLeast(1));
+
+    exptSet1 += EXPECT_CALL(*entidade1, loop()).Times(testing::AtLeast(1)).After(exptSet1);
+
+    exptSet2 += EXPECT_CALL(*entidade2, loop()).Times(testing::AtLeast(1)).After(exptSet2);
+
+    testing::Expectation expectation1 = EXPECT_CALL(*entidade1, finalization()).Times(testing::AtLeast(1)).After(exptSet1);
+
+    testing::Expectation expectation2 = EXPECT_CALL(*entidade2, finalization()).Times(testing::AtLeast(1)).After(exptSet2);
+
+    manager.addEntity(entidade1, 0);
+    manager.addEntity(entidade2, 0);
+
+    manager.startEntities();
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    manager.disableEntities();
 }
